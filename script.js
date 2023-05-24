0fd7a9cef1df4d004ecbd1e590c3cc51e039b544
leonardopea <leonardo.pea.1996@gmail.com>
2023-05-24 16:23:54 +0200

    esercizio terminato

--- script.js
+++ script.js
@@ -3,13 +3,19 @@ let lista_spesa = [
     'prosciutto' ,'mozzarella' , 'pane' , 'latte' , 'farina' , 'uova' , 'succo ace' , 'torta al limone' , 'arachidi'
 ] ;
 
-
+const alimenti = document.getElementById('alimenti');
 
 let i = 0;
 
 while (i < lista_spesa.length) {
 
-    console.log(lista_spesa[i])
+    let item = lista_spesa[i];
+
+    let list_item = document.createElement('li');
+
+    list_item.innerText = item
+    
+    alimenti.append(list_item)
 
     i++;
 

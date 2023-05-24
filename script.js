1e83c82d1dcf29b7d362b0ff54eac8161dd77d53
leonardopea <leonardo.pea.1996@gmail.com>
2023-05-24 16:06:12 +0200

    lista creata in console

---
+++ script.js
@@ -0,0 +1,16 @@
+
+let lista_spesa = [
+    'prosciutto' ,'mozzarella' , 'pane' , 'latte' , 'farina' , 'uova' , 'succo ace' , 'torta al limone' , 'arachidi'
+] ;
+
+
+
+let i = 0;
+
+while (i < lista_spesa.length) {
+
+    console.log(lista_spesa[i])
+
+    i++;
+
+} 
